be6e496904196a820749db04021414e66e5bbb44
ChaithanyaGandham <gandham.chaithanya.ui@gmail.com>
2024-01-01 17:40:06 -0600

    Added code for Nested HTML Template using React.

--- App.js
+++ App.js
@@ -10,9 +10,16 @@
 </div> */
 
 
-const heading = React.createElement("h1", 
-{ id: "heading" },
-"Hello world from React!");
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "chld" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "chld2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
