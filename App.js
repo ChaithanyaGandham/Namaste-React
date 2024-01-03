94e83ff17f65ca16e6ff87beb2a8606aac1d531d
ChaithanyaGandham <gandham.chaithanya.ui@gmail.com>
2024-01-03 13:20:09 -0600

    Episode 2: Ignite our App

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-/* <div id="parent">
-    <div id="child">
-        <h1>I am h1 tag</h1>
-        <h2>I am h2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>I am h1 tag</h1>
-        <h2>I am h2 tag</h2>
-    </div>
-</div> 
-
-ReactElement(Object) => HTML(Browser understands)
-*/
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "chld" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "This is namaste react"),
     React.createElement("h2", {}, "I am h2 tag"),
   ]),
   React.createElement("div", { id: "chld2" }, [
