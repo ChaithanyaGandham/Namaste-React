eebef20f59bb9e95d61a7e2ed1abb60c228b6b1d
ChaithanyaGandham <gandham.chaithanya.ui@gmail.com>
2024-01-04 19:07:11 -0600

    E3: Laying the foundation.

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "chld" }, [
-    React.createElement("h1", {}, "This is namaste react"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "chld2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
+// React Element
+const Title  = () => (
+    <h1 className="head" tabIndex="5">
+      Namaste React
+    </h1>
+);
+
+// React Functional Component.
+const HeadingComponent = () => {
+  return (
+    <div className='container'>
+      <Title /> 
+      <h1>Namaste React Functional Component</h1>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent/>);
